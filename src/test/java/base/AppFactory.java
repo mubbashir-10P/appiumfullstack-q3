5fd05151829d1d34d614e7b39f7f7266b10acc39
mubbashir.shakil <mubbashir.shakil@10pearls.com>
2024-10-25 10:05:19 +0500

    Session 5

--- src/test/java/base/AppFactory.java
+++ src/test/java/base/AppFactory.java
@@ -3,7 +3,10 @@ package base;
 import io.appium.java_client.AppiumDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Parameters;
+import utils.ConfigReader;
 
 import java.net.MalformedURLException;
 import java.net.URL;
@@ -11,29 +14,35 @@ import java.net.URL;
 public class AppFactory {
 
     public static AppiumDriver appiumDriver;
-
+    public static  ConfigReader configReader;
 
     @BeforeTest
-    public void initializer() throws MalformedURLException {
-        System.out.println("Initializing Appium Driver");
+    @Parameters({"platformName","platformVersion","deviceName"})
+    public void initializer(String platformName, String platformVersion, String deviceName) throws MalformedURLException {
+        try{
+            configReader = new ConfigReader();
+            System.out.println("Initializing Appium Driver");
 
-        DesiredCapabilities capabilities = new DesiredCapabilities();
+            DesiredCapabilities capabilities = new DesiredCapabilities();
 
-        capabilities.setCapability("deviceName", "emulator-5554");
-        capabilities.setCapability("automationName", "UiAutomator2");
-        capabilities.setCapability("platformName", "android");
-        capabilities.setCapability("platformVersion", "14");
-        capabilities.setCapability("app", System.getProperty("user.dir") + "/app/Android.SauceLabs.Mobile.Sample.app.2.7.1.apk");
-        capabilities.setCapability("noReset", "false");
-        capabilities.setCapability("appPackage","com.swaglabsmobileapp");
-        capabilities.setCapability("appActivity","com.swaglabsmobileapp.SplashActivity");
-        capabilities.setCapability("newCommandTimeout", "300");
+            capabilities.setCapability("platformName", platformName);
+            capabilities.setCapability("platformVersion", platformVersion);
+            capabilities.setCapability("deviceName", deviceName);
+            capabilities.setCapability("automationName", configReader.getAutomationName());
+            capabilities.setCapability("app", System.getProperty("user.dir") + configReader.getApkPath());
+            capabilities.setCapability("noReset", configReader.getNoReset());
+            capabilities.setCapability("appPackage",configReader.getAppPackage());
+            capabilities.setCapability("appActivity",configReader.getAppActivity());
+            capabilities.setCapability("newCommandTimeout", configReader.getCommandTimeoutValue());
 
-        appiumDriver = new AppiumDriver(new URL("http://127.0.0.1:4723"), capabilities);
+            appiumDriver = new AppiumDriver(new URL(configReader.appiumServerEndpointURL()), capabilities);
 
-        AppDriver.setDriver(appiumDriver);
-        System.out.println("Appium Driver Is Initialized Successfully!");
-
+            AppDriver.setDriver(appiumDriver);
+            System.out.println("Appium Driver Is Initialized Successfully!");
+        }catch (Exception e){
+            e.printStackTrace();
+            System.out.println("Failed to initialize Appium Driver!");
+        }
     }
 
     @AfterTest
